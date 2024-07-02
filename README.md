9c2692fb9b812e7aba1c68510a1770fe5b842081
miles <iy@berkeley.edu>
2024-07-02 14:34:35 -0700

    lots of stuff done

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Requires a configuration YAML file which contains several settings with the name
 - `Partition` is the partition to check 
 
 Card information should be placed in the YAML file after the settings and in the following format 
-<p align=center> <code>{"name":"2080ti", "min":2, "max":2, "weight":"2"}</code></p>
+<p align=center> <code>{"name":"GRTX2080TI", "min":2, "max":2, "weight":"2"}</code></p>
 
 Where `name` is the cards name in slurm gres, `min`/`max` is the minimum and maximum cpu that is required per card.
 `weight` is used to calculate the ratio and could be assigned by teraflops or VRAM or some sort of other preformance metric. When the ratio is enforced, the jobs ratio is calculated by multiplying the card count by its weight then dividing by total cpu count. For example if a user submits a job of `gpu:V100:4 ncpu = 4` and `weight` for the V100 card is set to `2` then the weight of the job will be `4 * 2 = 8` this is then divided by the number of cpus to determine the ratio `4/8 = 2`. A job submitted without specifying which card to be used (ex. gres `gpu:2`) will assume `DefaultCard`.
@@ -18,9 +18,9 @@ Where `name` is the cards name in slurm gres, `min`/`max` is the minimum and max
 Some pitfalls 
 - Errors if gres has gpu count greater than 9. I assume this won't be a problem since no node has double digit number of gpus? I also don't know how submissions work for jobs that require GPU spanning across nodes.
 - The YAML parser requires that the config has the 5 setting variables present in the order of the example config. And that GPU information is on the following line
-- Im fairly sure users can get around the entire plugin by submitting via the newer --gpu* flags rather than --gres. Ex user submits job as `sbatch --gpu 2080:2`
-- The user not specifying what gpu in gres means there is some guesswork in assuming which card will be used as the default. I don't know enough about slurm and how it chooses when GPU is not specified.
-
+- Im fairly sure users can get around the entire plugin by submitting via the newer `--gpu*` flags rather than `--gres`. Ex user submits job as `srun --gpu 2080:2`
+- The user not specifying what gpu in gres means there is some guesswork in assuming which card will be used as the default. I don't know enough about slurm and how it chooses this.
+- Slightly worried about memory stuffs since i was taught rust instead of C in class. `DefaultCard[20]` and `Partition[20]` are a bit generous but its fine.
 
 
 
--- work.c
+++ work.c
@@ -1,39 +1,70 @@
 work.c
 
+/* (Old header from original plugin)
+ * Copyright (c) 2016-2017, Yong Qin <yong.qin@lbl.gov>. All rights reserved.
+ *
+ * job_submit_require_cpu_gpu_ratio: Job Submit plugin to enfornce the
+ *      CPU/GPU ratio to facilitate accounting.
+ *
+ * This plugin enforces the job to provide enough CPU number to match the GPU
+ * number (e.g, 2:1) on a particular partition. If the provided number does not
+ * match the job will be refused.
+ *
+ * Note you will need to change the definition of "mypart" and "ratio" in the
+ * following code to meet your own requirement.
+ *
+ * gcc -shared -fPIC -pthread -I${SLURM_SRC_DIR}
+ *     job_submit_require_cpu_gpu_ratio.c -o job_submit_require_cpu_gpu_ratio.so
+ *
+ */
+
 #include <limits.h>
 #include <regex.h>
 #include <stdint.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define MAX_LINE_LENGTH 256
+#define MAX_ENTRIES 100
+
 // #include <slurm/slurm_errno.h>`
 // #include "src/slurmctld/slurmctld.h"
 
 /* Required by Slurm job_submit plugin interface. */
 const char plugin_name[] = "Require CPU/GPU ratio";
 const char plugin_type[] = "job_submit/require_cpu_gpu_ratio";
-// const uint32_t plugin_version = SLURM_VERSION_NUMBER;
+//const uint32_t plugin_version = SLURM_VERSION_NUMBER;
 
 /* Global variables. */
-const char *myname = "job_submit_require_cpu_gpu_ratio";
+const char *myname = "job_submit_require_cpu_gpu_ratio"; // slurm requires?
 const char *gpu_regex="^gpu:[_[:alnum:]:]*([[:digit:]]+)$"; // regex for gpu
+
 const int  npart = 1; // number of partitions to check
+const int *ratio = 2; // ratio required
 
-/* The Partition to be checked - need to modify. */
-const char *mypart[2] = {"es1"};
-/* The CPU/GPU ratio that is checked against - need to modify. */
-const int  ratio[2] = {2};
+/* Vars needed for config file */
+const char *config_file = "job_submit_ratio_config.yml"; // name of config file
+FILE *file;
+char line[MAX_LINE_LENGTH];
+char search_name[MAX_LINE_LENGTH];
 
-/* 
- Default values set below in case read from file is missing 
+/*  Config variables default values, updated when config is loaded*/
+int enforce_ratio = 0; 
+int enforce_min = 0; 
+int enforce_max = 0;  
+char default_card[20] = "V100";
+char partition[20] = "es1";
 
+struct Card {
+    char name[MAX_LINE_LENGTH];
+    int min;
+    int max;
+    char weight[MAX_LINE_LENGTH];
+};
 
-int enabled = 0;
-int maxA40 = 16;
-int maxV100 = 4;
-int max2080TI = 2;
-*/
+struct Card entries[MAX_ENTRIES];
+int num_entries = 0;
 
 /* Convert string to integer. */
 int _str2int (char *str, uint32_t *p2int) {
@@ -51,47 +82,88 @@ int _str2int (char *str, uint32_t *p2int) {
     return 0;
 }
 
-/* Parses the config file*/
-void parseConfigFile(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("Error opening file");
+// Function to parse a line containing JSON-like object
+void parse_line(char *line) {
+    if (num_entries >= MAX_ENTRIES) {
+        fprintf(stderr, "Max entries exceeded\n");
         return;
     }
 
-    char line[256];
-    while (fgets(line, sizeof(line), file)) {
-        // Remove newline character
-        line[strcspn(line, "\n")] = 0;
+    // Parse the line assuming fixed format
+    if (sscanf(line, "{\"name\":\"%[^\"]\", \"min\":%d, \"max\":%d, \"weight\":\"%[^\"]\"}",
+            entries[num_entries].name, &entries[num_entries].min,
+            &entries[num_entries].max, entries[num_entries].weight) == 4) {
+        num_entries++;
+    } else {
+        printf("Error parsing line: %s\n", line);
+    }
+}
 
-        // Check for enabled line
-        if (strncmp(line, "enabled=", 8) == 0) {
-            if (strcmp(line + 8, "True") == 0) {
-                enabled = 1;
-            } else {
-                enabled = 0;
-            }
+// Function to search for an entry by name
+struct Card* find_entry(const char *name) {
+    for (int i = 0; i < num_entries; ++i) {
+        if (strcmp(entries[i].name, name) == 0) {
+            return &entries[i];
+        }
+    }
+    return NULL;  // Card not found
+}
+
+void updateConfig(const char *line) {
+    // Use strtok to split the line on the colon
+    char key[50];
+    char value[50];
+    sscanf(line, "%[^:]:%s", key, value);
+
+    // Compare the key and update the corresponding variable
+    if (strcmp(key, "enforce_ratio") == 0) {
+        enforce_ratio = (strcmp(value, "True") == 0) ? 1 : 0;
+    } else if (strcmp(key, "default_card") == 0) {
+        strncpy(default_card, value, sizeof(default_card) - 1);
+        default_card[sizeof(default_card) - 1] = '\0'; // Ensure null-termination
+    } else if (strcmp(key, "enforce_min") == 0) {
+        enforce_min = (strcmp(value, "True") == 0) ? 1 : 0;
+    } else if (strcmp(key, "enforce_max") == 0) {
+        enforce_max = (strcmp(value, "True") == 0) ? 1 : 0;
+    } else if (strcmp(key, "partition") == 0) {
+        strncpy(partition, value, sizeof(partition) - 1);
+        partition[sizeof(partition) - 1] = '\0'; // Ensure null-termination
+    }
+}
+
+/* Parses the config file*/
+void readConfigFile(const char *filename) {
+
+    // Open the file
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+    
+    // Skip and parse the first 5 lines, update config global vars
+    for (int i = 0; i < 5; ++i) {
+        if (fgets(line, sizeof(line), file)) {
+            updateConfig(line);
         } else {
-            // Parse JSON-like line
-            char name[50];
-            int min, max, weight;
-
-            if (sscanf(line, "{\"name\":\"%[^\"]\", \"min\":%d, \"max\":%d, \"weight\":\"%d\"}", name, &min, &max, &weight) == 4) {
-                if (strcmp(name, "2080ti") == 0) {
-                    max2080TI = weight;
-                } else if (strcmp(name, "v100") == 0) {
-                    maxV100 = weight;
-                } else if (strcmp(name, "a40") == 0) {
-                    maxA40 = weight;
-                }
-            }
+            fprintf(stderr, "Error reading file or file has less than 5 lines.\n");
+            fclose(file);
+            return 1;
         }
     }
 
+    // Read each line and parse
+    while (fgets(line, sizeof(line), file)) {
+        // Remove newline character if present
+        line[strcspn(line, "\n")] = 0;
+        parse_line(line);
+    }
+
+    // Close the file
     fclose(file);
 }
 
-/* Main function*/
+/* Main function */
 int _check_ratio(char *part, char *gres, uint32_t ncpu) {
     if (part == NULL) {
         printf("%s: missed partition info", myname);
@@ -101,10 +173,10 @@ int _check_ratio(char *part, char *gres, uint32_t ncpu) {
     /* Loop through all partitions that need to be checked. */
     int i;
     for (i = 0; i < npart; i++) {
-        if (strcmp(part, mypart[i]) == 0) {
+        if (strcmp(part, partition[i]) == 0) {
             /* Require GRES on a GRES partition. */
             if (gres == NULL) {
-                printf("%s: missed GRES on partition %s", myname, mypart[i]);
+                printf("%s: missed GRES on partition %s", myname, partition[i]);
                 return 0;
             } else {
                 regex_t re;
@@ -123,25 +195,43 @@ int _check_ratio(char *part, char *gres, uint32_t ncpu) {
                 regfree(&re);
 
                 if (rv == 0) { /* match */
+
                     /* Convert the GPU # to integer. */
                     if (_str2int(gres + rm[1].rm_so, &ngpu) || ngpu < 1) {
                         printf("%s: invalid GPU number %s", myname, gres + rm[1].rm_so);
                         return 0;
                     }
+
+                    if (gres specifies a card) {
+                        if so update global var cardFound
+                    } else {
+                        update global var with default_card
+                    }
                     
-                    /* Escapes if enabled = 0*/
-                    if (enabled == 0) {
-                        return 1;
+                    /* Enforce minimum cpu requirements*/
+                    if (enforce_min) {
+                        if (ncpu is less than min * card count) {
+                            return 0;
+                        }
                     }
 
-                    /* Sanity check of the CPU/GPU ratio. */
-                    if (ncpu / ngpu <= 2) {
-                        printf("%s: CPU=%zu, GPU=%zu, not qualify", myname, ncpu, ngpu);
-                        return 0;
+                    /* Enforce maximum cpu requirements*/
+                    if (enforce_max) {
+                        if (ncpu is greater than max * card count) {
+                            return 0;
+                        }
                     }
 
+                    if (enforce_ratio) {
+                        if (ncpu / ngpu <= ratio) {
+                            // info("%s: CPU=%zu, GPU=%zu, not qualify", myname, ncpu, ngpu);
+                            printf("ratio not met");
+                            return 0;
+                        }
+                    }
+                
                 } else if (rv == REG_NOMATCH) { /* no match */
-                    printf("%s: missed GPU on partition %s", myname, mypart[i]);
+                    printf("%s: missed GPU on partition %s", myname, partition[i]);
                     return 0;
                 } else { /* error */
                     printf("%s: failed to match regex '%s': %m", myname, gpu_regex);
@@ -154,31 +244,15 @@ int _check_ratio(char *part, char *gres, uint32_t ncpu) {
     return 1;
 }
 
-extern int job_submit(struct job_descriptor *job_desc, uint32_t submit_uid,
-        char **err_msg) {
-    return _check_ratio(job_desc->partition,
-                        job_desc->tres_per_node,
-                        job_desc->min_cpus);
-}
-
-extern int job_modify(struct job_descriptor *job_desc,
-        struct job_record *job_ptr, uint32_t submit_uid) {
-    return _check_ratio(
-        job_desc->partition == NULL ? job_ptr->partition : job_desc->partition,
-        job_desc->tres_per_node == NULL ? job_ptr->tres_per_node : job_desc->tres_per_node,
-        job_desc->min_cpus == (uint32_t) -2 ? job_ptr->total_cpus :
-             job_desc->min_cpus);
-}
-
 int main(int argc, char *argv[]) {
     if (argc < 4) {
         printf("Usage: %s <arg1> <arg2> <arg3>\n", argv[0]);
         return 1;
     }
 
-    parseConfigFile("asdf.json");
+    readConfigFile(config_file);
 
-    printf("Partition: %s\n", argv[1]);
+    printf("partition: %s\n", argv[1]);
     printf("gres: %s\n", argv[2]);
     printf("ncpu: %s\n", argv[3]);
 
