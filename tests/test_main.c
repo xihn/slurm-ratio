aff14ecbae5f639fb73afc972d4132527d8f2c4d
miles <iy@berkeley.edu>
2024-07-19 18:20:04 -0700

    toml vs yaml

--- tests/test_main.c
+++ tests/test_main.c
@@ -17,17 +17,20 @@ void test_maxCpuCount(void) {
 }
 
 void test_minCpuCount(void) {
-    TEST_ASSERT_FALSE(_check_ratio("es1", "gpu:V100:2", 4));
+    TEST_ASSERT_TRUE(_check_ratio("es1", "gpu:V100:2", 4));
+    TEST_ASSERT_FALSE(_check_ratio("es1", "gpu:V100:2", 3));
 }
 
 void test_OutOfBoundsCpu(void) {
-
+    TEST_ASSERT_FALSE(_check_ratio("es1", "gpu:V100:2", 0));
+    TEST_ASSERT_FALSE(_check_ratio("es1", "gpu:V100:-1", 400));
+    TEST_ASSERT_FALSE(_check_ratio("es1", "gpu:V100:100", 6000));
 }
 
 // RATIO TESTS
  
 void test_ratio(void) {
-
+    TEST_ASSERT_EQUAL_STRING("Accepted", _check_ratio("es1", "gpu:A40:2", 2));
 }
 
 void test_ratioOutOfBounds(void) {
@@ -65,7 +68,8 @@ void test_noConfigFile(void) {
 // not needed when using
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_maxCpuCount);
-    RUN_TEST(test_minCpuCount);
+    //RUN_TEST(test_maxCpuCount);
+    //RUN_TEST(test_minCpuCount);
+    RUN_TEST(test_OutOfBoundsCpu);
     return UNITY_END();
 }
